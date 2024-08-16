e6e1c81fb234212e7794633a6f4de7622a9bc283
hekme5 <emile@bahvm18372823.resource.ds.bah.com>
2024-08-16 13:12:25 -0400

    Initial

---
+++ src/main/java/com/emile/spring/RedisString/RedisStringApplication.java
@@ -0,0 +1,13 @@
+package com.emile.spring.RedisString;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisStringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisStringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emile/spring/RedisString/RedisStringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emile.spring.RedisString;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisStringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
